0f0db4427339ffc77eff5f886a18f8dbbc3d48c9
Anjitha-A <120077993+Anjitha-A@users.noreply.github.com>
2023-01-25 19:03:10 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,4 @@
-import os
-from flask import Flask, render_template
-
-from flask_cors import CORS, cross_origin
-
+from flask import Flask
+from flask_cors import CORS
 app = Flask(__name__)
-
-
-app.secret_key = "super secret key"
-
 CORS(app)
