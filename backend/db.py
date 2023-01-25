8ea6cb8865f2d033952009f8c12c4daec81f9514
Anjitha-A <120077993+Anjitha-A@users.noreply.github.com>
2023-01-25 19:06:15 +0530

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -1,43 +1,33 @@
 import mysql.connector
-
 mydb = mysql.connector.connect(
-
     host = "localhost",
-
     user = "root",
-
     password = "password",
-
     database = "audiodatabase"
-
 )
+#create a table for storing the audio details in the database
 mydb_Create_Table_Query = """CREATE TABLE audio
 ( 
-  track_id int(100) not null auto_increment,
+  trackid int(100) not null auto_increment,
   title varchar(50) not null,
   category varchar(50) not null,
   author  varchar(50) not null,
   image varchar(250) not null,
   rating int(20),
-  CONSTRAINT audio_pk PRIMARY KEY (track_id)
+  CONSTRAINT audio_pk PRIMARY KEY (trackid)
 )"""
-
-
-
+# create a table for storing the details of users 
 mydb_Create_Table_Query= """CREATE TABLE user
-(email varchar(50) not null,
+(
+  userid int(100) not null auto_increment,
+  email varchar(50) not null,
   username varchar(50) not null,
   password varchar(50) not null,
-  usertype varchar(50) not null
+  usertype varchar(50) not null,
+  CONSTRAINT user_pk PRIMARY KEY (userid)
   
    )"""
-
-
-
-
-
+#create cursor 
 cursor = mydb.cursor()
-
 result = cursor.execute(mydb_Create_Table_Query)
-
 print(" Table created successfully ")
