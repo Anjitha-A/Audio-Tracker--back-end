be2f988431026f47fdc8960a332bc39a49053b47
Anjitha-A <120077993+Anjitha-A@users.noreply.github.com>
2023-01-25 19:05:42 +0530

    Update crud.py
    trying to make the functions(db connection making and closing) reusable. still have some errors, still working on it

--- backend/crud.py
+++ backend/crud.py
@@ -1,49 +1,25 @@
-import bcrypt
-import pymysql
 import json
+import jwt
+import pymysql
 from config import mydb
 from flask import jsonify
-from flask import flash, request
+from flask import request
 from app import app
-from flask_jwt_extended import JWTManager, get_jwt_identity, jwt_required, create_access_token
-
-
-
-
-
-
-from email.mime import image
-import pymysql
-import json
-from app import app
-from config import mydb
-from flask import jsonify
-from flask import flash, request
-
-
-
-
-
+from db_services import execute,closeConnection, cursor
 #insert audio details into audio table
 @app.route('/insert', methods=['POST'])
-def create_audio():
+def createAudio():
     try:
         json = request.json
         print(json)
-        # track_id= json['track_id']
         title = json['title']
         category = json['category']
         author = json['author']
         image = json['image']
-        
         if title and category and author and image and request.method == 'POST':
-            conn = mydb.connect()
-            cursor = conn.cursor(pymysql.cursors.DictCursor)
-            sqlQuery = "INSERT INTO audio(  title, category, author, image) VALUES( %s, %s, %s,%s)"
+            sqlQuery = "INSERT INTO audio( title, category, author, image) VALUES( %s, %s, %s,%s)"
             bindData = ( title, category, author, image)
-            cursor.execute(sqlQuery, bindData)
-            # print(cursor.execute(sqlQuery, bindData))
-            conn.commit()
+            execute(sqlQuery,bindData)
             respone = jsonify('Audio added successfully!')
             respone.status_code = 200
             return respone
@@ -52,15 +28,10 @@ def create_audio():
     except Exception as e:
         print(e)
         return 'Exception'
-    finally:
-        cursor.close()
-        conn.close()
 
-
-
-#view all datas in table
+#view all datas in audio table
 @app.route('/view_audios', methods =['GET'])
-def view_audios():
+def viewAudios():
     try:
         conn = mydb.connect()
         cursor = conn.cursor(pymysql.cursors.DictCursor)
@@ -71,19 +42,14 @@ def view_audios():
         return respone
     except Exception as e: 
         print(e)
-    finally:
-        cursor.close() 
-        conn.close() 
-
-
-
-#view particular data from table
-@app.route('/view_audio/<track_id>', methods=['GET'])
-def audio_details(track_id):
+     
+#view particular data from audio table
+@app.route('/view_audio/<trackid>', methods=['GET'])
+def audioAetails(trackid):
     try:
-        conn = mydb.connect()
-        cursor = conn.cursor(pymysql.cursors.DictCursor)
-        cursor.execute("SELECT track_id , title, category, author, image FROM audio WHERE track_id =%s", (track_id))
+        sqlQuery="SELECT trackid , title, category, author, image FROM audio WHERE trackid =%s"
+        bindData =(trackid)
+        execute(sqlQuery,bindData)
         empRow = cursor.fetchone()
         respone = jsonify(empRow)
         respone.status_code = 200
@@ -91,27 +57,21 @@ def audio_details(track_id):
     except Exception as e:
         print(e)
     finally:
-        cursor.close()
-        conn.close()
-
-
-
-
-
-#update audio
-@app.route('/update/<track_id>', methods=['PUT'])
-def update_audio(track_id):
+        closeConnection()
+#update audio from audio table
+@app.route('/update/<trackid>', methods=['PUT'])
+def updateAudio(trackid):
     try:
         _json = request.json
         print(_json)
-        _track_id = track_id
+        _track_id = trackid
         _title = _json['title']
         _category= _json['category']
         _author = _json['author']
         _image = _json['image']
        
         if  _title and _category and _author and  _image and request.method  == 'PUT':   
-            sqlQuery = ("UPDATE audio SET title= %s, category= %s, author= %s, image=%s  WHERE track_id=%s")
+            sqlQuery = ("UPDATE audio SET title= %s, category= %s, author= %s, image=%s  WHERE trackid=%s")
             bindData = ( _title, _category, _author,  _image,_track_id )
             conn = mydb.connect()
             cursor = conn.cursor()
@@ -125,72 +85,21 @@ def update_audio(track_id):
             return showMessage()
     except Exception as e:
         print(e)
-    finally:
-        cursor.close()
-        conn.close() 
-
-#delete track
-@app.route('/delete/<track_id>', methods=['DELETE'])
-def delete_audio(track_id):
+    
+#delete track from audio table
+@app.route('/delete/<trackid>', methods=['DELETE'])
+def deleteAudio(trackid):
     try:
-        conn = mydb.connect()
-        cursor = conn.cursor()
-        cursor.execute("DELETE FROM audio WHERE track_id =%s",(track_id))
-        conn.commit()
+        sqlQuery="DELETE FROM audio WHERE trackid =%s"
+        bindData=(trackid)
+        execute(sqlQuery,bindData)
         respone = jsonify('Audio deleted successfully!')
         respone.status_code = 200
         return respone
     except Exception as e:
         print(e)
-    finally:
-        cursor.close()
-        conn.close()
 
-
-
-
-#add ratings by user
-@app.route('/add_rating/<track_id>', methods=['PUT'])
-def add_ratings(track_id):
-    try:
-        json = request.json
-        print(json)
-        # track_id= json['track_id']
-        rating = json['rating']
-        if rating and request.method == 'PUT':
-            conn = mydb.connect()
-            cursor = conn.cursor(pymysql.cursors.DictCursor)
-            sqlQuery = "UPDATE audio SET rating=%s  WHERE track_id =%s"
-            bindData = (rating,track_id)
-            cursor.execute(sqlQuery, bindData)
-            # print(cursor.execute(sqlQuery, bindData))
-            conn.commit()
-            respone = jsonify('rating added successfully!')
-            respone.status_code = 200
-            return respone
-        else:
-            return showMessage()
-    except Exception as e:
-        print(e)
-        return 'Exception'
-    finally:
-        cursor.close()
-        conn.close()
-
-
-
-
-
-
-
-
-
-#login and signup
-
-@app.route('/get', methods=['GET'])
-def Get():
-    return 'haii'
-
+# signup for user , with email, username and password and datas are enetered to user table
 @app.route('/register', methods=['POST'])
 def register():
     try:
@@ -200,14 +109,9 @@ def register():
         username = json['username']
         password = json['password']
         usertype = "user"
-        # hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-        # print(hashed)
-      
         if email and username and password and usertype and request.method == 'POST':
-            
             conn = mydb.connect()
             cursor = conn.cursor(pymysql.cursors.DictCursor)
-            
             query= "SELECT * FROM user WHERE username= '%s'" % (username)
             data=cursor.execute(query)
             print(data)
@@ -232,8 +136,7 @@ def register():
     finally:
         cursor.close()
         conn.close()
-
-
+#Login function , which enable the user to login to the application using username and password
 @app.route('/login', methods=['POST'])
 def login():
     try:
@@ -241,23 +144,21 @@ def login():
         print(json)
         username = json['username']
         password = json['password']
-        # login_hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
         print(username)
         if username and password and request.method == 'POST':
             conn = mydb.connect()
             cursor = conn.cursor(pymysql.cursors.DictCursor)
-
             sqlQuery="SELECT * FROM user WHERE username= '%s'  and password='%s'" % (username, password)
-            # sqlQuery="SELECT * FROM user WHERE username= '%s' " % (username)
-            # print(sqlQuery)
             data=cursor.execute(sqlQuery)
-            row = cursor.fetchone()
-            usertype=row.get('usertype')         
+            print(data)      
             if data==1:
-                access_token = create_access_token(identity=username)
+                row = cursor.fetchone()
+                usertype=row.get('usertype') 
+                access_token = jwt.encode({
+                    'username': username
+                }, app.config['JWT_SECRET_KEY'])
                 conn.commit()
                 return jsonify(message='Login Successful', access_token=access_token ,usertype=usertype),200
-                
             else:
                 conn.commit()
                 return jsonify('Bad email or Password... Access Denied!'), 401
@@ -268,18 +169,8 @@ def login():
         return 'Exception'
     finally:
         cursor.close()
-        conn.close()
-
-
-            
-
-
-
-
-
-
-
-
+        conn.close()  
+#shows the exeption       
 @app.errorhandler(404)
 def showMessage(error=None):
     message = {
@@ -291,16 +182,7 @@ def showMessage(error=None):
     return respone
 
 
-
-@app.errorhandler(404)
-def showMessage(error=None):
-    message = {
-        'status': 404,
-        'message': 'Record not found: ' + request.url,
-    }
-    respone = jsonify(message)
-    respone.status_code = 404
-    return respone
+closeConnection()
 
 if __name__ == "__main__":
     app.run()
