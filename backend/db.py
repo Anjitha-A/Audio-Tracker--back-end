79e8b595fc86df66144a626939b0f2f8a5dcaf8b
Anjitha-A <120077993+Anjitha-A@users.noreply.github.com>
2023-01-30 21:09:54 +0530

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -1,5 +1,6 @@
-
+from socket import fromshare
 import mysql.connector
+from app import app
 
 mydb = mysql.connector.connect(
     host="localhost",
@@ -25,24 +26,30 @@ mydb_Create_Table_Query = """CREATE TABLE audio
   image varchar(250) not null,
   FOREIGN KEY (category) REFERENCES category(categoryid)
 )"""
-# # create role table which specifies the role id and role of user
+# # # create role table which specifies the role id and role of user
 mydb_Create_Table_Query = """CREATE TABLE role
 (
     roleid int(100) not null auto_increment,
     role varchar(50) not null,
     CONSTRAINT role_pk PRIMARY KEY (roleid)
  )"""
-# create a table for storing the details of users
+
+
+# # create a table for storing the details of users
 mydb_Create_Table_Query = """CREATE TABLE user
 (
   userid int(100) not null auto_increment PRIMARY KEY,
   fullname varchar(50) not null,
   username varchar(50) not null,
-  password varchar(50) not null,
+  password varchar(250) not null,
   usertype int(100) not null,
   FOREIGN KEY (usertype) REFERENCES role(roleid)
  )"""
-# # create table for add ratings
+
+
+
+
+# # # create table for add ratings
 mydb_Create_Table_Query = """CREATE TABLE rating
 (
     rateid int(100) not null auto_increment PRIMARY KEY,
