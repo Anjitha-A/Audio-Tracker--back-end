c00136b56e87f309653eeb0f57ef89375d17e6f0
Anjitha-A <120077993+Anjitha-A@users.noreply.github.com>
2023-01-30 17:31:54 +0530

    Update db_services.py

--- backend/db_services.py
+++ backend/db_services.py
@@ -4,9 +4,14 @@ conn = mydb.connect()
 cursor = conn.cursor(pymysql.cursors.DictCursor)
 #execute the sql query and commit the connection
 def execute(sqlQuery,bindData):
-    cursor.execute(sqlQuery, bindData)
+    data = cursor.execute(sqlQuery, bindData)
+    return data
+    
+#commit the connection
+def commitConnection():
     conn.commit()
+
 #close the connection   
 def closeConnection():
     cursor.close()
-    conn.close()
+    
