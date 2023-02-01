9c55657d7f94ee082bf05168b2ebbe0a472b2be7
Anjitha-A <120077993+Anjitha-A@users.noreply.github.com>
2023-02-01 12:10:17 +0530

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -1,5 +1,6 @@
 from app import app
 from views.admin.audio import *
+from views.admin.role import *
 from views.admin.category import *
 from views.user.user import *
 from views.user.rating import *
